4e9323101f0328df9d396bb72ac1b1e473bad4d7
jixiuf <jixiuf@qq.com>
2026-06-05 02:08:18 +0800

    keymap: collapse org-mode emphasis textobj bindings via pcase-dolist

--- helixel-keymap.el
+++ helixel-keymap.el
@@ -577,30 +577,19 @@ Example with multiple callbacks:
 ;; and derived modes, where ~...~ =...= _..._ /.../ *...* +...+ are
 ;; standard Org emphasis delimiters (code, verbatim, underline, italic,
 ;; bold, strikethrough).
-(helixel-define-key 'textobj-inner "~" #'helixel-mark-inner-tilde
-                     'org-mode)
-(helixel-define-key 'textobj-outer "~" #'helixel-mark-a-tilde
-                     'org-mode)
-(helixel-define-key 'textobj-inner "=" #'helixel-mark-inner-equal
-                     'org-mode)
-(helixel-define-key 'textobj-outer "=" #'helixel-mark-a-equal
-                     'org-mode)
-(helixel-define-key 'textobj-inner "_" #'helixel-mark-inner-underscore
-                     'org-mode)
-(helixel-define-key 'textobj-outer "_" #'helixel-mark-a-underscore
-                     'org-mode)
-(helixel-define-key 'textobj-inner "/" #'helixel-mark-inner-slash
-                     'org-mode)
-(helixel-define-key 'textobj-outer "/" #'helixel-mark-a-slash
-                     'org-mode)
-(helixel-define-key 'textobj-inner "*" #'helixel-mark-inner-star
-                     'org-mode)
-(helixel-define-key 'textobj-outer "*" #'helixel-mark-a-star
-                     'org-mode)
-(helixel-define-key 'textobj-inner "+" #'helixel-mark-inner-plus
-                     'org-mode)
-(helixel-define-key 'textobj-outer "+" #'helixel-mark-a-plus
-                     'org-mode)
+(pcase-dolist (`(,key . ,name)
+               '(("~" . "tilde")
+                 ("=" . "equal")
+                 ("_" . "underscore")
+                 ("/" . "slash")
+                 ("*" . "star")
+                 ("+" . "plus")))
+  (helixel-define-key 'textobj-inner key
+                       (intern (format "helixel-mark-inner-%s" name))
+                       'org-mode)
+  (helixel-define-key 'textobj-outer key
+                       (intern (format "helixel-mark-a-%s" name))
+                       'org-mode))
 
 ;; ── Hook registrations ──
 
